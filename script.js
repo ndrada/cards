ce7154394c97521fde5d77ff29caa611327120c8
ndrada <andrada.groseanu@gmail.com>
2023-09-22 11:22:25 -0700

    finished project

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementsByClassName("fab-fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.codecademy.https://www.youtube.com/watch?v=ZyhrYis509A/learn");
+});
+
+document.getElementsByClassName("fab-fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.codecademy.https://www.youtube.com/watch?v=ZyhrYis509A/learn");
+});
+
+document.getElementsByClassName("fab-fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.codecademy.https://www.youtube.com/watch?v=ZyhrYis509A/learn");
+});
+
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://www.https://www.news18.com/lifestyle/channeling-your-inner-barbie-a-journey-into-whimsical-fashion-and-fantasy-8405083.html.https://www.youtube.com/watch?v=ZyhrYis509A/learn");
+});
+
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://www.https://www.news18.com/lifestyle/channeling-your-inner-barbie-a-journey-into-whimsical-fashion-and-fantasy-8405083.html.https://www.youtube.com/watch?v=ZyhrYis509A/learn");
+});
+
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://www.https://www.news18.com/lifestyle/channeling-your-inner-barbie-a-journey-into-whimsical-fashion-and-fantasy-8405083.html.https://www.youtube.com/watch?v=ZyhrYis509A/learn");
+});
